668ccd3d4206174c29f83222f9a101b9703ffd63
Daniel <marillancadaniel@gmail.com>
2023-01-02 15:52:01 -0300

    se extrae variable appid

--- src/data/geolocation.js
+++ src/data/geolocation.js
@@ -1,6 +1,9 @@
+const appId = import.meta.env.VITE_API_KEY
+
+
 export async function getAddressLocations(ipAddress){
 
-    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_API_KEY}&ipAddress=${ipAddress}`;
+    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${appId}&ipAddress=${ipAddress}`;
     const respuesta = await fetch(url)
     const resultado = await respuesta.json()
 
